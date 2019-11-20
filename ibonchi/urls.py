e8255c300facbb969b56905faed3a7275d8ce431
karl thomas <k.bonnitto@gmail.com>
2019-11-20 04:38:36 -0500

    setup initial dashboard view

--- ibonchi/urls.py
+++ ibonchi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ibonchi.core import views
 
 urlpatterns = [
+    path('', views.dash, name='dash'),
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls'))
+
 ]
